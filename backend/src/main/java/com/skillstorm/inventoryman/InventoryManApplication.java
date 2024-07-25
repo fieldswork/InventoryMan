b133fec609059fb5833d67067bbc6a6be474c53b
Arthur Upfield <arthurupfield@outlook.com>
2024-07-25 17:05:02 -0400

    Starter

---
+++ backend/src/main/java/com/skillstorm/inventoryman/InventoryManApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.inventoryman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManApplication.class, args);
+	}
+
+}
