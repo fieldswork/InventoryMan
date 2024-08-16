9fba5674cd8e76a76cf7fcb93760fae3bd4e7d73
Arthur Upfield <arthurupfield@outlook.com>
2024-08-16 17:33:41 -0400

    Fixed previous commit
    Make branches for adding tests off of this one!

--- backend/src/test/java/com/skillstorm/cucumber/LoadPage.java
+++ backend/src/test/java/com/skillstorm/cucumber/LoadPage.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import com.skillstorm.selenium.InventoryManPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,48 +14,32 @@ public class LoadPage {
     private WebDriver driver;
     private InventoryManPage imPage;
 
-    @Before("@loading")
+    @Before
     public void before() {
-        System.out.println("Before hook: initializing WebDriver and InventoryManPage");
         ChromeOptions options = new ChromeOptions();
         this.driver = new ChromeDriver(options);
         this.imPage = new InventoryManPage(driver);
-        System.out.println("Before hook: InventoryManPage initialized: " + (this.imPage != null));
     }
 
     @Given("I attempt to load the website")
     public void loadWebsite() {
         System.out.println("Step: I attempt to load the website");
-        if (this.imPage == null) {
-            System.out.println("Error: InventoryManPage is null!");
-        } else {
-            this.imPage.get();
-        }
+        this.imPage.get();
     }
 
     @When("the website loads")
     public void websiteLoads() {
         System.out.println("Step: the website loads");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // Wait for the page to load
     }
 
     @Then("I should see the title {string}")
     public void checkTitle(String expectedTitle) {
         System.out.println("Step: I should see the title " + expectedTitle);
-        if (this.imPage == null) {
-            System.out.println("Error: InventoryManPage is null in checkTitle!");
-        } else {
-            String actualTitle = this.imPage.getTabTitle();
-            System.out.println("Actual title: " + actualTitle);
-            assertEquals(expectedTitle, actualTitle, "The page title should match the expected title");
-        }
+        assertEquals(expectedTitle, this.imPage.getTabTitle(), "The page title should match the expected title");
     }
 
-    @After("@loading")
+    @After
     public void tearDown() {
         if (driver != null) {
             this.driver.quit();
--- backend/src/test/java/com/skillstorm/selenium/InventoryManPage.java
+++ backend/src/test/java/com/skillstorm/selenium/InventoryManPage.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class InventoryManPage {
+
     private WebDriver driver;
     private static final String url = "http://localhost:3000";
 
     public InventoryManPage(WebDriver driver) {
         this.driver = driver;
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         PageFactory.initElements(driver, this);
     }
 
@@ -24,12 +25,6 @@ public class InventoryManPage {
     }
 
     public void get() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         this.driver.get(url);
     }
 }
-
