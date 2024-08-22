efc943b974674df1c98fcb62ed6cf11febf220ac
Arthur Upfield <arthurupfield@outlook.com>
2024-08-21 22:12:57 -0400

    Setting all tests to run

--- backend/src/test/java/com/skillstorm/cucumber/RunCucumberTest.java
+++ backend/src/test/java/com/skillstorm/cucumber/RunCucumberTest.java
@@ -10,8 +10,8 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-//@SelectClasspathResource("com/skillstorm/")
-@SelectClasspathResource("com/skillstorm/inventoryman/06_deleteWarehouse.feature")
+@SelectClasspathResource("com/skillstorm/")
+//@SelectClasspathResource("com/skillstorm/inventoryman/06_deleteWarehouse.feature")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.skillstorm.cucumber")
 public class RunCucumberTest {
