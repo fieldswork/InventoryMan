3f44bebce85142ded55b53a615900d69126755db
Arthur Upfield <arthurupfield@outlook.com>
2024-09-05 11:48:15 -0400

    Testing for presentation

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,6 +4,7 @@ const HomePage = () => {
   return (
     <div className="text-center">
       <h1>InventoryMan</h1>
+      <h1>Any questions?</h1>
       <p>A basic inventory management application created as part of the SkillStorm SDET Apprenticeship.</p>
       <p>Source for this project is available on <a href="https://github.com/salmoncore/InventoryMan">GitHub</a>!</p>
     </div>
